2df1d1e69cd71f85788f752330f9c9e320f3b635
daniel-zsigus <daniel.zsigus@everit.biz>
2014-03-28 11:55:55 +0100

    fixed licence headers

--- tests/config/src/main/java/org/everit/osgi/props/tests/config/PropertyComponentTestConfig.java
+++ tests/config/src/main/java/org/everit/osgi/props/tests/config/PropertyComponentTestConfig.java
@@ -18,14 +18,11 @@ package org.everit.osgi.props.tests.config;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 
 @Component(immediate = true)
 @Service(value = PropertyComponentTestConfig.class)
-@Properties({ @Property(name = "eosgi.testEngine", value = "junit4"),
-        @Property(name = "eosgi.testId", value = "PropertyComponentTest"),
-        @Property(name = "dataSource.target") })
+@Properties({})
 public class PropertyComponentTestConfig {
 
 }
